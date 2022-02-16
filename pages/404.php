b97951a4bd527ec50433ffd8df5e63aea16e430b
Wesley Souza <contato@wesleydesign.com.br>
2022-02-16 02:38:23 -0300

    Personalizando página 404

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,18 @@
-<h1>Página Não Encontrada</h1>
+<main class="bg-body-2-images py-5 bg-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+                <!-- card panel -->
+                <section class="card-panel">
+                    <header class="d-flex justify-content-md-between align-items-center flex-wrap">
+                        <h6 class="my-2 pr-3 pr-md-5">Página não encontrada - 404</h6>
+                    </header>
+
+                    <a href="/" class="btn bg-gradient-primary color-white border-none">Voltar a Home</a>
+                </section>
+
+            </div>
+        </div>
+    </div>
+</main>
